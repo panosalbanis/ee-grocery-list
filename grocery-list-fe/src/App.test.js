8eeb4bc28b61337622bae2f16f55b51c79edaa75
panosalbanis <panagiotisal@gmail.com>
2019-02-11 19:29:11 +0000

    Fixed App tests

--- grocery-list-fe/src/App.test.js
+++ grocery-list-fe/src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import GroceryLists from './components/GroceryLists/GroceryLists'
+import AddGroceryList from './components/AddGroceryList/AddGroceryList'
+import GroceryList from './components/GroceryList/GroceryList'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  it('renders renders a list of GroceryLists', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find(GroceryLists).length).toBe(1)
+  });
+
+  it('renders renders a list of AddGroceryList', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find(AddGroceryList).length).toBe(1)
+  });
+
+  it('renders renders a list of GroceryList', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find(GroceryList).length).toBe(1)
+  });
+})
